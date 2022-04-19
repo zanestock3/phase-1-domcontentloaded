8c08d7250a7c392f3bc023da3e31842251f802e9
Zane Stock <zanestock3@gmail.com>
2022-04-19 08:11:05 -0500

    commit tree cf348573b77f35f608ad9ee498df4149e45de352

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    const element = document.getElementById('text');
+    element.innerHTML = "This is really cool!";
+  });
